37ed50bc1450f3000c49600dd3d433bb38fd824e
Martin L. Jensen <martinloesethjensen@gmail.com>
2019-05-13 14:02:57 +0200

    Initial Commit

---
+++ favorite-movies-app/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  favorite-movies-app
+//
+//  Created by Martin Løseth Jensen on 13/05/2019.
+//  Copyright © 2019 Martin Løseth Jensen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
